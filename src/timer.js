74d2a4ad4a93035b05c3e67ab9b116479e5146ac
Will Hawkins <hawkinsw@borlaugic.com>
2018-12-26 14:27:18 -0500

    Let user change the number and start/stop the counter!

--- src/timer.js
+++ src/timer.js
@@ -6,25 +6,51 @@ class Timer extends React.Component {
 	console.log("Testing?");
 		this.state = {
 			integer_value: 65874,
+			button_title: "Start",
+			running: false,
 		}
 
 		this.intervalHandle;
 		this.handleInterval = this.handleInterval.bind(this);
-
-		this.intervalHandle = setInterval(this.handleInterval, 50);
+		this.handleStartPause = this.handleStartPause.bind(this);
+		this.handleDecimalChange = this.handleDecimalChange.bind(this);
 	}
 
-	handleInterval() {
-	console.log("testing?")
+	handleDecimalChange (event) {
 		this.setState({
-			integer_value: this.state.integer_value + 1
+			integer_value: event.target.value,
+			running: this.state.running,
+			button_title: this.state.button_title,
+		});
+	}
+
+	handleStartPause() {
+		if (this.state.running === true) {
+			this.setState({
+				integer_value: this.state.integer_value,
+				running: false,
+				button_title: "Start",
+			});
+			clearInterval(this.intervalHandle);
+		} else {
+			this.setState({
+				integer_value: this.state.integer_value,
+				running: true,
+				button_title: "Pause",
+			});
+			this.intervalHandle = setInterval(this.handleInterval, 1000);
+		}
+	}	
+	handleInterval() {
+		this.setState({
+			integer_value: parseInt(this.state.integer_value,10) + 1
 		})
 	}
 	render() {
 		return(
 		<div>
 			<div class="centerIt" id="number">
-			<p>{this.state.integer_value}</p>
+			<input type="text" value={this.state.integer_value} onChange={this.handleDecimalChange} />
 			</div>
 			<div class="row">
 			<div class="centerIt col-sm-2" id="q1">
@@ -49,6 +75,9 @@ class Timer extends React.Component {
 			<p>{this.state.integer_value % 256}</p>
 			</div>
 
+			</div>
+			<div class="centerIt" id="buttons">
+			<button onClick={this.handleStartPause}>{this.state.button_title}</button>
 			</div>
 		</div>
 		);
