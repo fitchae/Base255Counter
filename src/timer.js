d517ea6b68ef8c195531bac8c35f7e26198b897d
Will Hawkins <hawkinsw@borlaugic.com>
2018-12-27 20:48:38 -0500

    Temporarily commit dropdown changes so that I can change to the new repo.

--- src/timer.js
+++ src/timer.js
@@ -1,22 +1,108 @@
 const React = require('react')
 const ReactDom = require('react-dom')
+
+class Dropdown extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      listOpen: false,
+      headerTitle: this.props.title
+    }
+    this.close = this.close.bind(this)
+  }
+
+  componentDidUpdate(){
+    const { listOpen } = this.state
+    setTimeout(() => {
+      if(listOpen){
+        window.addEventListener('click', this.close)
+      }
+      else{
+        window.removeEventListener('click', this.close)
+      }
+    }, 0)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('click', this.close)
+  }
+
+  close(timeOut){
+    this.setState({
+      listOpen: false
+    })
+  }
+
+  selectItem(title, id, stateKey){
+    this.setState({
+      headerTitle: title,
+      listOpen: false
+    }, this.props.resetThenSet(id, stateKey))
+  }
+
+  toggleList(){
+    this.setState(prevState => ({
+      listOpen: !prevState.listOpen
+    }))
+  }
+
+  render(){
+    const{list} = this.props
+    const{listOpen, headerTitle} = this.state
+    return(
+      <div className="dd-wrapper">
+        <div className="dd-header" onClick={() => this.toggleList()}>
+          <div className="dd-header-title">{headerTitle}</div>
+          {listOpen
+            ? "X" //<FontAwesome name="angle-up" size="2x"/>
+            : "O" //<FontAwesome name="angle-down" size="2x"/>
+          }
+        </div>
+        {listOpen && <ul className="dd-list" onClick={e => e.stopPropagation()}>
+          {list.map((item)=> (
+            <li className="dd-list-item" key={item.id} onClick={() => this.selectItem(item.base, item.id, item.key)}>{item.base} {item.selected && "-" /*<FontAwesome name="check"/>*/}</li>
+          ))}
+        </ul>}
+      </div>
+    )
+  }
+}
 class Timer extends React.Component {
 	constructor(props) {
 		super(props)
-	console.log("Testing?");
 		this.state = {
 			integer_value: 65874,
 			button_title: "Start",
 			running: false,
+			base: [
+				{
+					id: 1,
+					base: 10,
+					selected: false,
+					name: "Ten"
+				},
+				{
+					id: 2,
+					base: 16,
+					selected: false,
+					name: "Sixteen"
+				},
+				{
+					id: 3,
+					base: 256,
+					selected: true,
+					name: "Two hundred fifty six."
+				}
+			],
 		}
-
 		this.intervalHandle;
 		this.handleInterval = this.handleInterval.bind(this);
 		this.handleStartPause = this.handleStartPause.bind(this);
 		this.handleDecimalChange = this.handleDecimalChange.bind(this);
+		this.resetThenSet = this.resetThenSet.bind(this);
 	}
 
-	handleDecimalChange (event) {
+	handleDecimalChange(event) {
 		this.setState({
 			integer_value: event.target.value,
 			running: this.state.running,
@@ -45,6 +131,14 @@ class Timer extends React.Component {
 		this.setState({
 			integer_value: parseInt(this.state.integer_value,10) + 1
 		})
+	}
+	resetThenSet(id, key) {
+    let temp = JSON.parse(JSON.stringify(this.state[key]))
+    temp.forEach(item => item.selected = false);
+    temp[id].selected = true;
+    this.setState({
+      [key]: temp
+    })
 	}
 	render() {
 		return(
@@ -79,6 +173,10 @@ class Timer extends React.Component {
 			</div>
 			<div class="centerIt" id="buttons">
 			<button class="btn btn-success" onClick={this.handleStartPause}>{this.state.button_title}</button>
+			<Dropdown
+            title="Select base"
+            list={this.state.base}
+            resetThenSet={this.resetThenSet}/>
 			</div>
 		</div>
 		);
