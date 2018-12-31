21de1ee4fb9072a92ffac9e6be6332092e1c78ff
Will Hawkins <hawkinsw@borlaugic.com>
2018-12-30 19:54:28 -0500

    Change design and refactor to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FontAwesome from 'react-fontawesome'
 const React = require('react')
 const ReactDom = require('react-dom')
 
@@ -6,7 +7,9 @@ class Dropdown extends React.Component {
     super(props)
     this.state = {
       listOpen: false,
-      headerTitle: this.props.title
+      headerTitle: this.props.list.filter(function (item) {
+				return item.selected === true;
+			})[0].base
     }
     this.close = this.close.bind(this)
   }
@@ -35,7 +38,7 @@ class Dropdown extends React.Component {
 
   selectItem(title, id, stateKey){
     this.setState({
-      headerTitle: title,
+      headerTitle: this.props.list[id].base,
       listOpen: false
     }, this.props.resetThenSet(id, stateKey))
   }
@@ -54,42 +57,46 @@ class Dropdown extends React.Component {
         <div className="dd-header" onClick={() => this.toggleList()}>
           <div className="dd-header-title">{headerTitle}</div>
           {listOpen
-            ? "X" //<FontAwesome name="angle-up" size="2x"/>
-            : "O" //<FontAwesome name="angle-down" size="2x"/>
+            ? <FontAwesome name="angle-up" size="2x"/>
+            : <FontAwesome name="angle-down" size="2x"/>
           }
         </div>
         {listOpen && <ul className="dd-list" onClick={e => e.stopPropagation()}>
           {list.map((item)=> (
-            <li className="dd-list-item" key={item.id} onClick={() => this.selectItem(item.base, item.id, item.key)}>{item.base} {item.selected && "-" /*<FontAwesome name="check"/>*/}</li>
+            <li className="dd-list-item" key={item.id} onClick={() => this.selectItem(item.base, item.id, item.key)}>{item.base} {item.selected && <FontAwesome name="check"/>}</li>
           ))}
         </ul>}
       </div>
     )
   }
 }
-class Timer extends React.Component {
+class App extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			integer_value: 65874,
 			button_title: "Start",
 			running: false,
+			selected_base: 256,
 			base: [
 				{
-					id: 1,
+					id: 0,
 					base: 10,
+					key: 'base',
 					selected: false,
 					name: "Ten"
 				},
 				{
-					id: 2,
+					id: 1,
 					base: 16,
+					key: 'base',
 					selected: false,
 					name: "Sixteen"
 				},
 				{
-					id: 3,
+					id: 2,
 					base: 256,
+					key: 'base',
 					selected: true,
 					name: "Two hundred fifty six."
 				}
@@ -132,54 +139,71 @@ class Timer extends React.Component {
 			integer_value: parseInt(this.state.integer_value,10) + 1
 		})
 	}
+
 	resetThenSet(id, key) {
     let temp = JSON.parse(JSON.stringify(this.state[key]))
+		console.log("temp: " + JSON.stringify(temp))
     temp.forEach(item => item.selected = false);
     temp[id].selected = true;
     this.setState({
       [key]: temp
     })
+		this.setState({
+			selected_base: parseInt(temp[id].base, 10),
+			integer_value: this.state.integer_value,
+		})
 	}
+
+	format(number, base) {
+		console.log("number: " + number)
+		console.log("base: " + base)
+		if (base === 16)
+			return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][number];
+		else
+			return number
+	}
+
 	render() {
 		return(
 		<div>
-			<h1 id="centerTitle">Base X Counter</h1>
+			<h1 id="centerTitle">Base {this.state.selected_base} Counter</h1>
 			<div class="centerIt" id="number">
 			<input class="form-control" id="numberValue" type="text" value={this.state.integer_value} onChange={this.handleDecimalChange} />
+/*		I'd like a newline here. */
+			Base: <Dropdown
+            list={this.state.base}
+            resetThenSet={this.resetThenSet}/>
+/*		I'd like a newline here. */
+			<button class="btn btn-success" onClick={this.handleStartPause}>{this.state.button_title}</button>
 			</div>
 			<div class="row">
 			<div class="centerIt col-sm-2" id="q1">
-			<p>{(parseInt(this.state.integer_value/(256*256*256), 10)) % 256}</p>
+			<p>{ this.format(parseInt((this.state.integer_value / Math.pow(this.state.selected_base, 3)) % this.state.selected_base, 10), parseInt(this.state.selected_base, 10)) }</p>
 			</div>
 			<div class="centerIt col-sm-1">
 			.
 			</div>
 			<div class="centerIt col-sm-2" id="q2">
-			<p>{(parseInt(this.state.integer_value/(256*256), 10)) % 256}</p>
+			<p>{ this.format(parseInt((this.state.integer_value / Math.pow(this.state.selected_base, 2)) % this.state.selected_base, 10), parseInt(this.state.selected_base, 10)) }</p>
 			</div>
 			<div class="centerIt col-sm-1">
 			    .
 			</div>
 			<div class="centerIt col-sm-2" id="q3">
-			<p>{(parseInt(this.state.integer_value/256, 10)) % 256}</p>
+			<p>{ this.format(parseInt((this.state.integer_value / Math.pow(this.state.selected_base, 1)) % this.state.selected_base, 10), parseInt(this.state.selected_base, 10)) }</p>
 			</div>
 			<div class="centerIt col-sm-1">
 			    .
 			</div>
 			<div class="centerIt col-sm-2" id="q4">
-			<p>{this.state.integer_value % 256}</p>
+			<p>{this.format(this.state.integer_value % this.state.selected_base, parseInt(this.state.selected_base, 10))}</p>
 			</div>
 
 			</div>
 			<div class="centerIt" id="buttons">
-			<button class="btn btn-success" onClick={this.handleStartPause}>{this.state.button_title}</button>
-			<Dropdown
-            title="Select base"
-            list={this.state.base}
-            resetThenSet={this.resetThenSet}/>
 			</div>
 		</div>
 		);
 	}
 }
-export default Timer
+export default App
